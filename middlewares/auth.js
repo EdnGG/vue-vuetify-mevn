b2856b196ec69e4ec708c0cbf22fa6bf0bccfb82
Eden <gresseden@gmail.com>
2021-09-06 21:40:20 -0400

    done most issues

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,13 +2,14 @@ const jwt = require('jsonwebtoken')
 
 const verificarAuth = (req, res, next) => {
   const token = req.get('token')
+  console.log('Token ', token)
 
   console.log('Token on backend: ', token)
   jwt.verify(token, process.env.SECRETORPRIVATEKEY, (err, decoded) => {
 
     if (err) {
       return res.status(401).json({
-        mensaje: 'Invalid user',
+        message: 'Invalid user',
       })
     }
     // Adding the 'user' propertiy to the 'req' object
