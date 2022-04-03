5d97d38932ee6a216fd26a3ce85b60fe0870c044
Eden <gresseden@gmail.com>
2022-04-03 11:06:01 -0400

    enable/disable sorting on frontend side

--- controllers/auth.js
+++ controllers/auth.js
@@ -181,7 +181,7 @@ export const forgotPassword = async (req, res = Response) => {
                 html: `
             <h2>Please click on the given link to reset your password</h2>
             
-            <a href="${process.env.CLIENT_URL}/reset-password/${formattingToken}">
+            <a href="${process.env.CLIENT_URL_CLOUD}/reset-password/${formattingToken}">
             Click here to reset your password
             </a> 
             `
